f93659c1724b58994f04deae0a668369213311d8
Milan Mishra <milanmishr20@gmail.com>
2023-12-29 04:52:03 -0600

    Exec.

---
+++ MyElectronApp-win32-x64/resources/app/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: path.join(__dirname, 'AlgaeBird.ico') // Adjust the file name and format
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
